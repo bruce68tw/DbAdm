0030be36b3de964e77fd59ced29fa71bdc708879
bruce <bruce68tw@gmail.com>
2026-08-20 15:35:37 +0800

    modify _WebExt.cs

--- Program.cs
+++ Program.cs
@@ -24,13 +24,13 @@ builder.Configuration.GetSection("MyConfig").Bind(myConfig);
 _Xp.Config = myConfig;
 
 //set builder
-builder.SetBuilder();
+builder.SetMyBuilder();
 #endregion
 
 #region 2.set services
 var multiLang = false;
 var services = builder.Services;
-services.SetServices(multiLang);
+services.SetMyServices(multiLang);
 
 services.AddSingleton<IBaseUserSvc, MyBaseUserSvc>();   //user info for base component
 services.AddTransient<DbConnection, SqlConnection>();   //ado.net for mssql
@@ -49,6 +49,6 @@ var isDev = app.Environment.IsDevelopment();
 _Fun.Init(isDev, app.Services, DbTypeEnum.MSSql, AuthTypeEnum.Row, multiLang);
 await _Locale.SetCultureA(_Fun.Config.Locale);
 
-app.SetApp(isDev);
+app.SetMyApp(isDev);
 app.Run();
 #endregion
